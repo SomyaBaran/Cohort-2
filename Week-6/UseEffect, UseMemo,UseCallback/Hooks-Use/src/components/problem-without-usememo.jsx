8edae6a2e4aaf58f643817c28af9544a016b3581
Somya Baranwal <116670796+SomyaBaran@users.noreply.github.com>
2025-06-22 21:17:49 +0530

    Add files via upload

---
+++ Week-6/UseEffect, UseMemo,UseCallback/Hooks-Use/src/components/problem-without-usememo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+    const [input, setInput] = useState(0);
+    const [output, setOutput] = useState(null);
+
+    function handleInputChange(e) {
+        setInput(Number(e.target.value));
+    }
+    function inbox() {
+        let newCount = 0;
+        for (let i = 0; i <= input; i++) {
+            newCount = newCount + i;
+        }
+        setOutput(newCount);
+    }
+    function counter() {
+        setCount(count + 1)
+    }
+    return (
+        <div>
+            <input
+                placeholder="Enter a number"
+                type="number"
+                value={input}
+                onChange={handleInputChange}
+            />
+            <button onClick={inbox}>Click</button>
+            <p>Sum {input} is {output}</p>
+            <button onClick={counter}>Counter {count}</button>
+        </div>
+    )
+}
+
+export default App;
+
+
+// This code is slightly unoptimistic because everytime App renders (when counter number changes) input is also changed --> slightly bad practice --> that's why we use useMemo. 
