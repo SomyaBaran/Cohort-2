45f409d4915fbd2eeb2614659d37395daa978f3d
Somya Baranwal <116670796+SomyaBaran@users.noreply.github.com>
2025-08-10 22:02:28 +0530

    Intro to Generics

---
+++ Week-9/Enums-Generic/src/Generic.ts
@@ -0,0 +1,9 @@
+// GENERICS
+/// This is just for Understanding
+
+// Generics allow you to create reusable and type-safe components that work with any data type without losing information about that type.
+// Why use Generics?
+
+// 1.  Reusability – Write once, work with any type.
+// 2. Type safety – Prevents accidental type errors.
+// 3. Better code readability.
